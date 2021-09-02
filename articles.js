75a4c89c67176f2273b6783991fdafdfc2d05ad4
Kiko Čabala <kristiancabala@gmail.com>
2021-09-02 20:30:17 +0200

    initial commit

---
+++ articles.js
@@ -0,0 +1,48 @@
+const articles = {
+  "non-academic": [
+    {
+      "title": "No Meetings, No Deadlines, No Full-Time Employees",
+      "href": "https://sahillavingia.com/work",
+      "prio": 5
+    },
+    {
+      "title": "Consume less, create more",
+      "href": "https://blog.tjcx.me/p/consume-less-create-more",
+      "prio": 2
+    },
+    {
+      "title": "How I experience web today",
+      "href": "https://how-i-experience-web-today.com/",
+    },
+    {
+      "title": "What I've Learned in 45 Years in the Software Industry",
+      "href": "https://www.bti360.com/what-ive-learned-in-45-years-in-the-software-industry"
+    },
+    {
+      "title": "Reverse Engineering the source code of the BioNTech/Pfizer SARS-CoV-2 Vaccine",
+      "href": "https://berthub.eu/articles/posts/reverse-engineering-source-code-of-the-biontech-pfizer-vaccine/"
+    },
+    {
+	"title": "Unoriginal Rice Patty (Honda fob replay based attack)",
+	"href": "https://github.com/HackingIntoYourHeart/Unoriginal-Rice-Patty"
+    },
+  ],
+  "resources": [
+	 {
+		"title": "Build your own X",
+		 "href": "https://github.com/danistefanovic/build-your-own-x"
+	 }
+  ],
+  "academic": [
+    {
+      "title": "Better Science Through Art",
+      "href": "https://dreamsongs.com/Files/BetterScienceThroughArt.pdf"
+    },
+	  {
+		  "title": "Why Functional Programming Matters",
+		  "href": "https://www.cs.kent.ac.uk/people/staff/dat/miranda/whyfp90.pdf",
+		  "prio": 5
+	  }
+
+  ],
+}
